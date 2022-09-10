4c3f6d320d76c4be98782c4935443f05c7307653
Ibi <phenixibm@gmail.com>
2022-09-10 15:29:52 +0100

    mise en place des assets front

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import { registerReactControllerComponents } from '@symfony/ux-react';
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
+
---
+++ assets/front/app.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+const  Popper = require('popper.js').default;
+
+window.$ = window.jQuery = $;
+
+import './css/styles.css';
+
+import 'bootstrap';
+import './js/slick';
+import './js/slider-bg';
+import './js/smoothproducts';
+import './js/snackbar.min';
+import './js/jQuery.style.switcher';
+import './js/custom';
